31a41c2357805d3b905e4b66d2bf959c1b0ebeb2
Dhakshnamoorthy Murugesan <81191286+DhakshnamoorthyM-DevOps@users.noreply.github.com>
2026-09-08 18:54:20 +0100

    GH-1: Updating ec2.tf

--- terraform/modules/ec2/ec2.tf
+++ terraform/modules/ec2/ec2.tf
@@ -7,18 +7,7 @@ resource "aws_instance" "ec2_instance" {
   key_name                = var.key_name
   iam_instance_profile    = var.iam_instance_profile
   vpc_security_group_ids  = var.sg_ids
-  user_data               = <<EOF
-#!/bin/bash
-sudo hostnamectl set-hostname ${var.ec2_name}
-sudo useradd ssm-user
-sudo echo 'ssm-user ALL=(ALL) NOPASSWD:ALL'  > /etc/sudoers.d/ssm-agent-users
-sudo chmod 440 /etc/sudoers.d/ssm-agent-users
-TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/
-IDENTITY_DOC="$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" "http://169.254.169.254/latest/dynamic/instance-identity/document")"
-TAG_NAME="$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" "http://169.254.169.254/latest/meta-data/tags/instance/Name")"
-sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sudo systemctl restart sshd
-	EOF
+  user_data               = local.user_data
   disable_api_termination = var.disable_api_termination
   metadata_options {
     http_endpoint          = "enabled"
